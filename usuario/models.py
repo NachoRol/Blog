a45971ee0eb93f907fd8f7553bd3ebfe557996e4
Jose Ignacio <ignacio.rolfi00@gmail.com>
2025-04-10 19:17:01 -0300

    Uso de ADMIN

--- usuario/models.py
+++ usuario/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 class Pais(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return f"{self.nombre.capitalize()}"
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    fecha_nacimiento = models.DateTimeField(null=True,blank=True)
+    fecha_nacimiento = models.DateField(null=True,blank=True)
     pais_actual = models.ForeignKey(Pais, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return f"{self.apellido.upper()},{self.nombre.capitalize()}"
     
 
 # Create your models here.
